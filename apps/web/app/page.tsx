0dd8016135721f5d3e560e2bebbcfd842855f4d5
hyeonjun-L <beeminor60@gmail.com>
2025-05-22 17:01:13 +0900

    api-etc: 모노레포 web 서비스용 Dockerfile 및 빌드 최적화 적용

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
-import { z } from "zod";
+import z from "zod";
 
 type Props = Omit<ImageProps, "src"> & {
   srcLight: string;
