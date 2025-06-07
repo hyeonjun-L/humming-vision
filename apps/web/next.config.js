94e434371981d24b5fa4aa3514e1f96ec772507d
duckoo <beeminor60@gmail.com>
2025-06-08 02:03:05 +0900

    web-etc: svgr 설치 및 설정

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
 };
 
 export default nextConfig;
