f8ff0c8642f7f2ab2491be68bfbfcbffb56a6e0b
duckoo <beeminor60@gmail.com>
2025-07-12 02:45:10 +0900

    web-feat: 메인 화면 제품 반응형 구현

--- apps/web/app/_components/product.tsx
+++ apps/web/app/_components/product.tsx
@@ -41,6 +41,20 @@ function Product() {
       ],
     },
     {
+      image: lightHero,
+      name: "Light",
+      hrefs: [
+        {
+          name: "Light",
+          href: RoutePath.LIGHT,
+        },
+        {
+          name: "Download",
+          href: `${RoutePath.LIGHT}${RoutePathWithCategory.DOWNLOAD}`,
+        },
+      ],
+    },
+    {
       image: frameGrabberHero,
       name: "Frame Grabber",
       hrefs: [
@@ -62,20 +76,7 @@ function Product() {
         },
       ],
     },
-    {
-      image: lightHero,
-      name: "Light",
-      hrefs: [
-        {
-          name: "Light",
-          href: RoutePath.LIGHT,
-        },
-        {
-          name: "Download",
-          href: `${RoutePath.LIGHT}${RoutePathWithCategory.DOWNLOAD}`,
-        },
-      ],
-    },
+
     {
       image: etcHero,
       name: "Etc",
@@ -93,32 +94,34 @@ function Product() {
   ];
 
   return (
-    <section className="flex w-full flex-col items-center justify-center">
-      <h2 className="text-gray600 mt-32 mb-24 text-[50px] font-bold">
+    <section className="flex w-full flex-col items-center justify-center py-10 md:py-20 lg:py-32">
+      <h2 className="text-gray600 mb-10 text-[30px] font-bold md:mb-[60px] md:text-[40px] lg:mb-24 lg:text-[50px]">
         Product
       </h2>
-      <ul className="flex flex-wrap justify-center gap-5">
+      <ul className="flex flex-wrap justify-center gap-5 sm:mx-10 lg:mx-0">
         {PRODUCT.map(({ image, name, hrefs }, index) => (
           <li key={index} className="flex flex-col items-center">
-            <div className="relative mx-16 my-4 aspect-square h-[184px]">
+            <div className="relative mx-9 my-4 aspect-square h-24 md:mx-12 md:h-32 lg:mx-16 lg:h-[184px]">
               <Image
                 src={image}
                 alt={`Product ${index + 1}`}
                 fill
-                sizes="184px"
+                sizes="(max-width: 768px) 96px, (max-width: 1024px) 128px, 184px"
                 className="object-cover"
               />
             </div>
-            <div className="bg-main flex h-16 w-full items-center justify-center text-[28px] font-bold text-white">
+            <div className="bg-main flex h-8 w-full items-center justify-center text-lg font-bold text-white md:h-10 md:text-[20px] lg:h-16 lg:text-[28px]">
               {name}
             </div>
             {hrefs.map(({ name, href }) => (
               <Link
                 key={name}
                 href={href}
-                className="border-main text-main flex w-full items-center justify-between border-b py-5"
+                className="group border-main text-main flex w-full items-center justify-between border-b px-2.5 py-2 md:py-2.5 lg:px-[20px] lg:py-[20px]"
               >
-                <span className="px-4 py-2 text-xl font-light">{name}</span>
+                <span className="text-sm font-light group-hover:font-medium md:text-base lg:text-xl">
+                  {name}
+                </span>
                 <PluseSVG />
               </Link>
             ))}
