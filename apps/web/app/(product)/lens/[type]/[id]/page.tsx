fccc5b6e901b6316b0f28ebf2242072f8eefd1e9
duckoo <beeminor60@gmail.com>
2025-07-25 23:17:01 +0900

    web-feat: lens, frmae grabber 제품 상세 페이지 구현

---
+++ apps/web/app/(product)/lens/[type]/[id]/page.tsx
@@ -0,0 +1,33 @@
+import Detail from "@/(product)/_components/detail";
+import { LensProduct } from "@humming-vision/shared";
+import axios from "axios";
+import { ENV_API_END_POINT_KEY } from "consts/env-keys.const";
+import { handleApiError } from "utils/api-error-handler";
+
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+async function page({ params }: Props) {
+  const { id } = await params;
+
+  const END_POINT = process.env[ENV_API_END_POINT_KEY];
+
+  try {
+    const lensData = await axios.get<LensProduct>(
+      `${END_POINT}/product/lens/${id}`,
+      {
+        adapter: "fetch",
+        fetchOptions: {
+          cache: "force-cache",
+        },
+      },
+    );
+
+    return <Detail product={lensData.data} />;
+  } catch (error) {
+    handleApiError(error);
+  }
+}
+
+export default page;
