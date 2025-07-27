86fc24f7f5ef8d8aba1178eb2cb9777c2c604514
duckoo <beeminor60@gmail.com>
2025-07-28 03:16:54 +0900

    web-fix: 무한 리디렉션 수정 테스트

--- apps/web/utils/verify-access-token.ts
+++ apps/web/utils/verify-access-token.ts
@@ -6,7 +6,9 @@ async function verifyAccessToken(token: string) {
   const secret = process.env[ENV_JWT_SECRET_KEY];
   if (!secret) throw new Error("JWT 시크릿 키가 설정되지 않았습니다.");
 
-  const { payload } = await jwtVerify(token, new TextEncoder().encode(secret));
+  const { payload } = await jwtVerify(token, new TextEncoder().encode(secret), {
+    clockTolerance: "5s",
+  });
 
   return payload as Record<string, Admin>;
 }
