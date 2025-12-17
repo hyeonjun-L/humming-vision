eb3f94f10a3c4bc443896968e256ea537446043c
duckoo <beeminor60@gmail.com>
2025-12-17 21:48:11 +0900

    web-refactor: 네이버 길찾기 icon 변경

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -37,7 +37,7 @@ const nextConfig = {
       ),
       new URL("https://humming-vision-s3-bucket.s3.amazonaws.com/**"),
       new URL("https://map.kakao.com/favicon.ico"),
-      new URL("https://map.naver.com/p/assets/icons/favicon.ico"),
+      new URL("https://ssl.pstatic.net/static/maps/assets/icons/favicon.ico"),
     ],
   },
 };
